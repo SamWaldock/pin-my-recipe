1d10fe77789d78ad660985a1c78c0a9f6ee946df
Sam Waldock <sam.waldock@student.vives.be>
2021-10-10 15:37:28 +0200

    Adding some more info & layout to the recipe detail page

--- components/recipe-detail.js
+++ components/recipe-detail.js
@@ -4,11 +4,22 @@ import { RichText } from 'prismic-reactjs'
 import styles from '../styles/recipe-detail.module.css'
 
 export default function RecipeDetail({ recipe }) {
+  console.log(recipe.chef.name)
   return (
     <section className={styles.detail}>
       <RichText render={recipe.name} />
       <RichText render={recipe.description} />
       <div className={styles.image} style={{backgroundImage: "url(" + recipe.picture.url + ")"}}></div>
+      <article className={styles.chef}>
+        <Image
+          width={50}
+          height={50}
+          alt={`foto of the chef`}
+          className={styles.imageChef}
+          src={recipe.chef.picture.url}
+        />
+        <span className={styles.chefName}>By {recipe.chef.name}</span>
+      </article>
       <article>
         <h2>Ingredients</h2>
         <RichText render={recipe.ingredients} />
