f7847d0f82a5b00dbd69f024ac4a72622cb297a4
Sam Waldock <sam.waldock@student.vives.be>
2021-10-11 08:16:37 +0200

    Changing the footer text

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p className={styles.description}>&copy; Copyright 2021 Pin My Recipe. All Rights Reserved.</p>
+      <p className={styles.description}>2021 Pin My Recipe.</p>
     </footer>
   )
 }
